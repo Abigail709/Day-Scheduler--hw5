e36c5c800ca45aa76b52a279e59e398cb6957ab6
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-06 23:46:24 -0500

    finish js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,9 +4,53 @@ $("#currentDay").text(dayScheduler);
 $("#currentDay").text(moment().format("dddd, MMM Do"));
 
 $(".saveBtn").on("click", function () {
-    console.log(this);
+   
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, text);
 })
+
+function timeTracker() {
+    
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+        var time = parseInt($(this).attr("id").split("hour")[1])
+        console.log(time, currentTime)
+
+        if (time < currentTime) {
+            
+            $(this).removeClass("future");
+        }
+        else if (time === currentTime) {
+            
+            $(this).addClass("present");
+           
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+           
+
+        };
+    });
+};
+
+
+
+function savelocal() {
+    $("#nueve .description").val(localStorage.getItem("nueve"));
+    $("#diez .description").val(localStorage.getItem("diez"));
+    $("#once .description").val(localStorage.getItem("once"));
+    $("#doce .description").val(localStorage.getItem("doce"));
+    $("#uno .description").val(localStorage.getItem("uno"));
+    $("#dos .description").val(localStorage.getItem("dos"));
+    $("#tres .description").val(localStorage.getItem("tres"));
+    $("#cuatro .description").val(localStorage.getItem("cuatro"));
+    $("#cinco .description").val(localStorage.getItem("cinco"));
+
+}
+savelocal();
+timeTracker();
